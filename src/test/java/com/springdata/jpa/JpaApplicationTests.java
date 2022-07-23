d1472e8a045fee8cdb67a05d2446afdd403b2f05
Dmitry Berzhanin <di.berzhanin@gmail.com>
2022-07-24 02:00:55 +0300

    Add SQL scripts for creating DB, users and tests for database

--- src/test/java/com/springdata/jpa/JpaApplicationTests.java
+++ src/test/java/com/springdata/jpa/JpaApplicationTests.java
@@ -1,13 +1,26 @@
 package com.springdata.jpa;
 
+import com.springdata.jpa.repositories.BookRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JpaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    BookRepository bookRepository;
 
+    @Test
+    void testBookRepository() {
+        long count = bookRepository.count();
+
+        assertThat(count).isGreaterThan(0);
+    }
+
+    @Test
+    void contextLoads() {
+    }
 }
---
+++ src/test/java/com/springdata/jpa/SpringBootJpaTestSlice.java
@@ -0,0 +1,44 @@
+package com.springdata.jpa;
+
+import com.springdata.jpa.domain.Book;
+import com.springdata.jpa.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ComponentScan(basePackages = {"com.springdata.jpa.bootstrap"})
+public class SpringBootJpaTestSlice {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @Test
+    @Commit
+    @Order(1)
+    @Rollback(value = false)
+    void testJpaTestSlice() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(2);
+
+        bookRepository.save(new Book("My Book", "123456", "Self"));
+        long countAfter = bookRepository.count();
+        assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Test
+    @Order(2)
+    void testJpaTestSliceTransaction() {
+        long countBefore = bookRepository.count();
+        assertThat(countBefore).isEqualTo(3);
+    }
+}
